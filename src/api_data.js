336539bfdbac8fc2bd433e0ecc4eaddfe0669280
Juan Diego Guerra <jdxelric@gmail.com>
2023-05-23 09:41:17 -0500

    Update api_data.js
    POST method

--- src/api_data.js
+++ src/api_data.js
@@ -13,4 +13,18 @@ export default class API {
       const response = await fetch(url);
       return response.json();
     }
+  
+    async post(url, data) {
+      this.url = url;
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify(data),
+      });
+      const res = await response;
+      return res;
+    }
   }
