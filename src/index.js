182364760ed0f000af82f82cfabe128853532542
lincoln <lincolngibson7@gmail.com>
2023-05-24 01:21:16 -0300

    Removed comments and moved api call to header file

--- src/index.js
+++ src/index.js
@@ -1,32 +1,28 @@
 import './style.css';
 import createHeader from './modules/header.js';
-// import API from './modules/api_data.js';
-import pokemons from './modules/pokemons.js';
 import likesPost, { likeGet } from './modules/likes.js';
 
 createHeader();
 
-const itemNumbers = document.getElementById('PokeNumber');
-likeGet()
-  .then((data) => {
-    data.forEach((element) => {
-      const targetItem = element[1].item_id.split('likeId').slice(1);
-      document.querySelector(`#like-count${targetItem[0]}`).innerHTML = `${element[1].likes} Likes`;
+document.addEventListener('DOMContentLoaded', () => {
+  likeGet()
+    .then((data) => {
+      data.forEach((element) => {
+        const targetItem = element[1].item_id.split('likeId').slice(1);
+        const targetElement = document.querySelector(`#like-count${targetItem[0]}`);
+        if (targetElement) {
+          targetElement.innerHTML = `${element[1].likes} Likes`;
+        }
+      });
     });
+
+  document.body.addEventListener('click', (e) => {
+    if (e.target && e.target.className === 'likeBtn') {
+      likesPost(e.target);
+      likeGet();
+      const targetId = (e.target.id).split('likeId').slice(1);
+      const targetHeart = document.querySelector(`#like-count${targetId}`).innerHTML.split(' ');
+      document.querySelector(`#like-count${targetId}`).innerHTML = `${(Number(targetHeart[0])) + 1} Likes`;
+    }
   });
-
-document.body.addEventListener('click', (e) => {
-  if (e.target && e.target.className === 'likeBtn') {
-    likesPost(e.target);
-    likeGet();
-    const targetId = (e.target.id).split('likeId').slice(1);
-    const targetHeart = document.querySelector(`#like-count${targetId}`).innerHTML.split(' ');
-    document.querySelector(`#like-count${targetId}`).innerHTML = `${(Number(targetHeart[0])) + 1} Likes`;
-  }
 });
-
-pokemons('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0').then((response) => {
-  itemNumbers.innerHTML = `Pokemons(${response})`;
-});
-
-// const api = new API();
