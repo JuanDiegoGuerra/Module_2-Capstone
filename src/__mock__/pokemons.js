a0169cb820a1559a28db7c8f17fa6a9d31abacbc
Juan Diego Guerra <jdxelric@gmail.com>
2023-05-25 15:11:46 -0500

    Create new Unit Test for Pokemon Counter

--- src/__mock__/pokemons.js
+++ src/__mock__/pokemons.js
@@ -1,18 +1,2 @@
-import fetch from 'node-fetch';
-
-let numberOfItems;
-
-const pokemons = async (url) => {
-  await fetch(url).then((response) => response.json())
-    .then((data) => data.results)
-    .then((obj) => Object.entries(obj))
-    .then((arr) => {
-      numberOfItems = arr.length;
-    })
-    .catch((err) => {
-      throw ("Couldn't fetch pokemons", err);
-    });
-  return numberOfItems;
-};
-
-export default pokemons;
+const pokeCounter = (array) => array.length;
+export default pokeCounter;
--- src/__test__/pokemonsCounter.test.js
+++ src/__test__/pokemonsCounter.test.js
@@ -1,21 +1,16 @@
-import pokemons from '../__mock__/pokemons.js';
+import pokeCounter from '../__mock__/pokemons.js';
 
-describe('Test number of items on Home Page', () => {
-  test('False Pokemons count with limit 15 and expect 30', () => {
-    pokemons('https://pokeapi.co/api/v2/pokemon?limit=15&offset=0').then((response) => {
-      expect(response).toBe(30);
-    });
+describe('Pokemon Counter Test', () => {
+  test('Tests when the Pokemon Obj has some value', () => {
+    const items = [{ item: 1 }, { item: 2 }, { item: 3 }, { item: 4 }, { item: 5 }];
+    expect(pokeCounter(items)).toBe(5);
   });
-
-  test('Pokemons count with limit 25', () => {
-    pokemons('https://pokeapi.co/api/v2/pokemon?limit=25&offset=0').then((response) => {
-      expect(response).toBe(25);
-    });
+  test('Test when the Pokemon Obj is empty', () => {
+    const items = [];
+    expect(pokeCounter(items)).toBe(0);
   });
-
-  test('Pokemons count with limit 35', () => {
-    pokemons('https://pokeapi.co/api/v2/pokemon?limit=35&offset=0').then((response) => {
-      expect(response).toBe(35);
-    });
+  test('Test when the Pokemon Obj has one value', () => {
+    const items = [{}];
+    expect(pokeCounter(items)).toBe(1);
   });
 });
