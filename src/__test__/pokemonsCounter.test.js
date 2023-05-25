69675de0347f69421dc76bb92d3cd76f61f1d26e
Juan Diego Guerra <jdxelric@gmail.com>
2023-05-25 13:26:55 -0500

    Create pokemonsCounter.test.js
    Pokemons Home Page counter Test
    3 tests made

---
+++ src/__test__/pokemonsCounter.test.js
@@ -0,0 +1,21 @@
+import pokemons from "../__mock__/pokemons.js";
+
+describe('Test number of items on Home Page', () => {
+    test('False Pokemons count with limit 15 and expect 30', () => {
+        pokemons('https://pokeapi.co/api/v2/pokemon?limit=15&offset=0').then((response) => {
+          expect(response).toBe(30);
+        });
+      });
+
+      test('Pokemons count with limit 25', () => {
+        pokemons('https://pokeapi.co/api/v2/pokemon?limit=25&offset=0').then((response) => {
+          expect(response).toBe(25);
+        });
+      });
+      
+      test('Pokemons count with limit 35', () => {
+        pokemons('https://pokeapi.co/api/v2/pokemon?limit=35&offset=0').then((response) => {
+          expect(response).toBe(35);
+        });
+      });
+});
